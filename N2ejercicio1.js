daeccadd7bf212a02908de4b5f51e773b606be75
pater <agustinpaternoster@gmail.com>
2023-03-01 08:05:27 +0100

    Ejercicio 3 con

---
+++ N2ejercicio1.js
@@ -0,0 +1,26 @@
+/*Donats els objectes employees i salaries, crea una arrow function 
+getEmployee() que retorni una Promise efectuant la cerca en l'objecte pel seu id.*/
+
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+console.log("test")
